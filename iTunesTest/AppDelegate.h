b5a6cd266b8cfa36fda74cfe01617aade5eaa811
colin <hitwhylz@163.com>
2014-06-08 19:12:09 +0800

    Initial Commit

---
+++ iTunesTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iTunesTest
+//
+//  Created by Colin on 14-6-8.
+//  Copyright (c) 2014年 icephone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
